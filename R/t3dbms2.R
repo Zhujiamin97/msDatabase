7ce317be0b98057d2773795af683ab717e340c29
ZHUJIAMIN <93595586+Zhujiamin97@users.noreply.github.com>
2022-10-18 12:04:35 +0800

    Update t3dbms2.R

--- R/t3dbms2.R
+++ R/t3dbms2.R
@@ -1,17 +1,6 @@
 # Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+
+
 get_t3db <- function(page_num=c("45"),name=c("pollutant"),sleep_time=c("2")){
   library(tidyverse)
   library(rvest)
