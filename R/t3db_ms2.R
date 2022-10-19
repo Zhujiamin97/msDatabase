8035ea732dfb4a4207ccee568afd9f9b5d2d39da
ZHUJIAMIN <93595586+Zhujiamin97@users.noreply.github.com>
2022-10-19 21:19:31 +0800

    Update t3db_ms2.R

--- R/t3db_ms2.R
+++ R/t3db_ms2.R
@@ -40,7 +40,7 @@ get_t3db <- function(start_page_num=c("1"),end_page_num=c("2"),
   }) %>% do.call(rbind,.)
 
   close(pb)
-  if（dir.exist（“./T3DB”）==假){
+  if（dir.exist（“./T3DB”）==FALSE){
 dir.create（“./T3DB”)
   }
   file.name <- paste0(name,"_t3dID")
