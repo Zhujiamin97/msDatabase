e2544ac9f3d1629ca2941eb6bb88f97d11617b41
ZHUJIAMIN <93595586+Zhujiamin97@users.noreply.github.com>
2022-11-09 19:39:27 +0800

    Update GNPS_data.R

--- R/GNPS_data.R
+++ R/GNPS_data.R
@@ -1,61 +1,72 @@
 
-#file <- "F:/桌面/R开发/数据库操作/HMDB.msp"
-gnps_ms2 <- function(file){
+#file <- "F:/HMDB.msp"
+GNPS_ms2 <- function (file){
   library(tidyverse)
   message("正在读取msp数据")
-  msp.data <- readr::read_lines(file,progress = FALSE)
+  msp.data <- readr::read_lines(file, progress = FALSE)
+  #检查一下文件中名称的形式
+  test_index<- grep("NAME",msp.data[1])
+  if(length(test_index)>0){
+    colname <- c("NAME","PRECURSORMZ","PRECURSORTYPE","FORMULA","IONMODE","COLLISIONENERGY")
+    ms2_type <- "2"
+  }else{
+    colname <- c("Name","PrecursorMZ","Precursor_type","Formula","Ion_mode","Collision_energy")
+    ms2_type <- "1"
+  }
   message("读取完成,转换中")
-  #show tail data
-  #tail(msp.data,100)
-  #head(msp.data,100)
-  #header with whitespace
-  msp.data <- unlist(list("",msp.data))
-  blank_index <- which(msp.data=="")
-  #检查一下blank是否为偶数
- if(length(blank_index)%%2==!0 ){
-   stop("信息缺失")
- }
-  num <- 1:(length(blank_index)/2)##代表一共有多少个化合物
+  msp.data <- unlist(list("", msp.data))
+  blank_index <- which(msp.data == "")
+  if (length(blank_index)%%2 == !0) {
+    stop("信息缺失")
+  }
+  num <- 1:(length(blank_index)/2)
   pb <- txtProgressBar(0, length(num), style = 3)
-  gnps_ms2 <- lapply(num, function(x){
+  gnps_ms2 <- lapply(num, function(x) {
     setTxtProgressBar(pb, x)
-    compound_data <-msp.data[blank_index[-1+2*x]:blank_index[0+2*x]]
-    info<- compound_data[grep(": ",compound_data)]
-    ms2 <- compound_data[grep("\t",compound_data)]
-    #字符串提取info
-    
-    info_all <- lapply(1:length(info), FUN = function(x){
-      
+    compound_data <- msp.data[blank_index[-1 + 2 * x]:blank_index[0 +                                                                2 * x]]
+    info <- compound_data[grep(": ", compound_data)]
+    info_index <- grep(": ", compound_data)
+    #ms2 <- compound_data[grep("\t", compound_data)]
+   
+    info_all <- lapply(1:length(info), FUN = function(x) {
       j <- strsplit(info[x], ": ")
-      info <- sapply(j, "[",1)
-      value <- sapply(j, "[",2)
-      info_all <- cbind(info,value)
-    }) %>% do.call(rbind,.) %>% as.data.frame() 
-    NAME <- filter(info_all,info_all$info==c("NAME"))[[2]]
-    PRECURSORMZ <- filter(info_all,info_all$info==c("PRECURSORMZ"))[[2]]
-    PRECURSORTYPE <- filter(info_all,info_all$info==c("PRECURSORTYPE"))[[2]]
-    FORMULA <- filter(info_all,info_all$info==c("FORMULA"))[[2]]
-    IONMODE <- filter(info_all,info_all$info==c("IONMODE"))[[2]]
-    #COLLISIONENERGY <- filter(info_all,info_all$info==c("COLLISIONENERGY"))[[2]]
-    compound_info <- data.frame(nameE=NAME ,precursor=PRECURSORMZ,
-                                type=PRECURSORTYPE,formula=FORMULA,
-                                ionmode=IONMODE)
-    ##字符串提取 ms2
-    msms <- lapply(1:length(ms2), FUN = function(x){
-      j <- strsplit(ms2[x], "\t")
-      mz <- sapply(j, "[",1) 
-      intensity <- sapply(j, "[",2) 
-      #list(data.frame(mz=mz,intensity=intensity))
-      cbind(mz,intensity)
-    }) %>% do.call(rbind,.) %>% as.data.frame() 
-    gnps_ms2 <- compound_info %>% mutate(ms2=list(data.frame(mz=msms$mz,intensity=msms$intensity)))
-  }) %>% do.call(rbind,.)
-  
-  if(dir.exists("./GNPS")==FALSE){
+      info <- sapply(j, "[", 1)
+      value <- sapply(j, "[", 2)
+      info_all <- cbind(info, value)
+    }) %>% do.call(rbind, .) %>% as.data.frame()
+    compound_info <- lapply(colname, function(x){
+      a <- filter(info_all, info_all$info == x)[[2]]
+      if(length(a)==0){
+        a="NA"
+      }
+      a
+    }) %>% do.call(cbind,.) %>% as.data.frame()
+    colnames(compound_info) <- colname
+    if(ms2_type==1){
+      ms2 <- compound_data[c(-1,-length(compound_data),-info_index)]
+      msms <- lapply(1:length(ms2), FUN = function(x) {
+        j <- strsplit(ms2[x], " ")
+        mz <- sapply(j, "[", 1)
+        intensity <- sapply(j, "[", 2)
+        cbind(mz, intensity)
+      }) %>% do.call(rbind, .) %>% as.data.frame()
+    }else{
+      ms2 <- compound_data[grep("\t", compound_data)]
+      msms <- lapply(1:length(ms2), FUN = function(x) {
+        j <- strsplit(ms2[x], "\t")
+        mz <- sapply(j, "[", 1)
+        intensity <- sapply(j, "[", 2)
+        cbind(mz, intensity)
+      }) %>% do.call(rbind, .) %>% as.data.frame()
+    }
+    
+    gnps_ms2 <- compound_info %>% mutate(ms2 = list(data.frame(mz = msms$mz, 
+                                                               intensity = msms$intensity)))
+  }) %>% do.call(rbind, .)
+  if (dir.exists("./GNPS") == FALSE) {
     dir.create("./GNPS")
   }
-  save(gnps_ms2,file = "./GNPS/gnps_ms2.Rdata")
+  save(gnps_ms2, file = "./GNPS/gnps_ms2.Rdata")
   message("数据库构建完成,已保存至默认路径")
   gnps_ms2
 }
-
